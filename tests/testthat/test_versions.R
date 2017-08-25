0475fc7468463469e2caa742262306756ccb3f4b
Laurent <lg390@cam.ac.uk>
2017-08-25 16:39:46 +0100

    new pkg version unit test

---
+++ tests/testthat/test_versions.R
@@ -0,0 +1,24 @@
+context("Package versions")
+
+test_that("Package versions", {
+    def <- biocpkgversion("MSnbase")
+    rel <- biocpkgversion("MSnbase", "release")
+    dev <- biocpkgversion("MSnbase", "devel")
+    expect_identical(def, rel)
+    expect_false(is.null(names(dev)))
+
+    rel <- biocpkgversion("MSnbase", USE.NAMES = FALSE)
+    dev <- biocpkgversion("MSnbase", "devel", USE.NAMES = FALSE)
+    expect_true(is.null(names(dev)))
+    rel <- new("Versioned", versions = list(MSnbase = rel))
+    dev <- new("Versioned", versions = list(MSnbase = dev))
+    expect_true(classVersion(rel) < classVersion(dev))
+    gh <- githubpkgversion("lgatto/MSnbase", USE.NAMES = FALSE)
+    gh <- new("Versioned", versions = list(MSnbase = gh))
+    expect_true(classVersion(dev) <= classVersion(gh))
+
+    ps <- c("MSnbase", "pRoloc")
+    expect_identical(length(ps), length(biocpkgversion(ps)))
+    expect_identical(length(ps),
+                     length(githubpkgversion(paste0("lgatto/", ps))))    
+})
